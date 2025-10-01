a28d59b200c7d98d000228fdc8e411f833aea303
Aleks <develop@MacBook-Air-Aleks.local>
2025-10-01 17:45:09 +0300

    create App.jsx

---
+++ my-react-router-app/src/App.jsx
@@ -0,0 +1,32 @@
+import { NavLink, Outlet } from "react-router-dom";
+
+import routes from "./routes/routes.jsx";
+
+export default function App() {
+    return (
+        <div>
+            <header>
+                <nav className="menu">
+                    {routes[0].children.map((route, index) => {
+                        if (route.index) {
+                            return (
+                                <NavLink key={index} to="/" end>
+                                    Головна
+                                </NavLink>
+                            );
+                        }
+                        return (
+                            <NavLink key={index} to={`/${route.path}`}>
+                                {route.label}
+                            </NavLink>
+                        );
+                    })}
+                </nav>
+            </header>
+
+            <main>
+                <Outlet />
+            </main>
+        </div>
+    );
+}
