419a006f32e59b703411db4d356fa00679bc5323
Aleks <develop@MacBook-Air-Aleks.local>
2025-10-01 17:33:29 +0300

    create components

---
+++ my-react-router-app/src/components/Contact.jsx
@@ -0,0 +1,8 @@
+export default function Contact() {
+    return (
+        <div className="page">
+            <h2>Contact</h2>
+            <p>Send to email</p>
+        </div>
+    );
+}
---
+++ my-react-router-app/src/components/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div className="page">
+            <h2>Hello!</h2>
+            <p>Demo React Content</p>
+        </div>
+    );
+}
